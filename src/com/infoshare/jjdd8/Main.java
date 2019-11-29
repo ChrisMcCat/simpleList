ab4cc3fa9360c82c1aa54fa526f4bf5cd0c74f02
Chris <ichris.kk@gmail.com>
2019-11-29 11:45:19 +0100

    simple list

---
+++ src/com/infoshare/jjdd8/Main.java
@@ -0,0 +1,23 @@
+package com.infoshare.jjdd8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+	List<String> words = new ArrayList<>();
+
+	words.add("quick");
+	words.add("brown");
+	words.add("fox");
+
+        System.out.println(words);
+
+        System.out.println(words.contains("fox"));
+
+        System.out.println(words.isEmpty());
+
+        System.out.println(words.size() == 0);
+    }
+}
